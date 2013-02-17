b722cb6452d1dcb1404284ccb7fa4aa653ad68a8
Chaya <Chaya@chaya-sima>
2013-02-16 20:57:55 -0500

    Added random to color and gridlines in Projectile

--- src/spiegel/parabola/GraphComponent.java
+++ src/spiegel/parabola/GraphComponent.java
@@ -20,21 +20,24 @@ public class GraphComponent extends JComponent {
 	private static final long serialVersionUID = 1L;
 	private double time = 0;
 	private Projectile[] arcs;
+	private Random randomNum;
 
 	public GraphComponent() {
-		Random randomNum = new Random();
+		randomNum = new Random();
 
 		arcs = new Projectile[5];
 		for (int i = 0; i < arcs.length; i++) {
 			Projectile a = new Projectile(randomNum.nextInt(180),
-					randomNum.nextInt(500), color(i));
+					randomNum.nextInt(500), color());
 			arcs[i] = a;
 		}
 	}
-//testing
+
+	// testing
 	// override method- here we can fill a paint component
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		gridlines(g);
 		g.translate(getWidth() / 2, getHeight() / 2);
 		time += .001;
 		for (int i = 0; i < arcs.length; i++) {
@@ -43,28 +46,38 @@ public class GraphComponent extends JComponent {
 			// obviously could call from this class too
 			g.setColor(arcs[i].getC());
 			g.fillOval(x1, y1, 10, 10);
+
 		}
+
 		this.repaint();
 	}
-	public void trails()
-	{
-		
+
+	public void trails() {
+
 	}
-	public void gridlines()
-	{
-		
+
+	public void gridlines(Graphics g) {
+		int height = 0, width = 0;
+		while (width <= getWidth()) {
+			g.drawLine(width, height, width, getHeight());
+			width += 20;
+		}
+		height = 0;
+		width = 0;
+		while (height <= getHeight()) {
+			g.drawLine(width, height, getWidth(), height);
+			height += 20;
+		}
 	}
-	public void randomColors()
-	{
-		
+
+	public void continuousLifespan() {
+
 	}
-	public void continuousLifespan()
-	{
-		
-	}
-	public Color color(int i) {
-		i += 1;
-		Color j = new Color(i * 40, i * 20, i * 32, i * 50);
+
+	public Color color() {
+
+		Color j = new Color(randomNum.nextInt(255), randomNum.nextInt(255),
+				randomNum.nextInt(255));
 		return j;
 	}
 
